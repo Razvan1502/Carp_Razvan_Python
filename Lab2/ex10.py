5a7c3b81f88a36383ad36b3b036d213f59792253
Razvan Carp <carprazvan02@gmail.com>
2023-10-30 14:28:16 +0200

    lab2

--- Lab2/ex10.py
+++ Lab2/ex10.py
@@ -1,20 +1,21 @@
-def zip_lists(*lists):
+def function10(*lists):
     max_length = max(len(lst) for lst in lists)
-    zipped = []
+    result = []
 
     for i in range(max_length):
         tuple_items = tuple(lst[i] if i < len(lst) else None for lst in lists)
-        zipped.append(tuple_items)
+        result.append(tuple_items)
 
-    return zipped
+    return result
 
 def main():
     list1 = [1, 2, 3]
     list2 = [5, 6, 7]
     list3 = ["a", "b", "c"]
 
-    result = zip_lists(list1, list2, list3)
+    result = function10(list1, list2, list3)
     print(result)
+
 
 if __name__ == "__main__":
     main()
